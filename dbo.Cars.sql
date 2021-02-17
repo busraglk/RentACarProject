fef8a6f04a19120b0f2a02fb799da3a150ff5574
busraglk <busragelik@gmail.com>
2021-02-17 03:54:59 +0300

    Db

--- dbo.Cars.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Cars]
-(
-	[CarId] INT NOT NULL PRIMARY KEY, 
-    [BrandId ] INT NULL, 
-    [ColorId] INT NULL, 
-    [ModelYear ] INT NULL, 
-    [DailyPrice] DECIMAL(18, 2) NULL, 
-    [Description] VARCHAR(100) NULL
-)
