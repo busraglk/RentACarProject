7aa205128c47d9b6c8b3509745ec4ce39b38ec21
busraglk <busragelik@gmail.com>
2021-03-03 04:22:43 +0300

    3.03.2021

--- RecapProjectDb.sql
+++ RecapProjectDb.sql
@@ -1,45 +1,68 @@
-CREATE TABLE Colors(
-	Id int PRIMARY KEY IDENTITY(1,1),
-	[Name] nvarchar(50),
-)
+CREATE TABLE [dbo].[Cars] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [BrandId]     INT            NOT NULL,
+    [ColorId]     INT            NOT NULL,
+    [ModelYear]   INT            NULL,
+    [DailyPrice]  DECIMAL (18)   NULL,
+    [Description] NVARCHAR (200) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([Id])
+);
 
-CREATE TABLE Brands(
-	Id int PRIMARY KEY IDENTITY(1,1),
-	[Name] nvarchar(50),
-)
+CREATE TABLE [dbo].[Brands] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [BrandName] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
 
-CREATE TABLE Cars(
-	Id int PRIMARY KEY IDENTITY(1,1),
-	Name nvarchar(50),
-	BrandId int,
-	ColorId int,
-	DailyPrice decimal,
-	ModelYear int,
-	Description nvarchar(50),
-	FOREIGN KEY (ColorId) REFERENCES Colors(Id),
-	FOREIGN KEY (BrandId) REFERENCES Brands(Id)
-)
-CREATE TABLE Users(
-	Id int PRIMARY KEY IDENTITY(1,1),
-	FirstName nvarchar(50),
-	LastName nvarchar(50),
-	Email nvarchar(50),
-	Password nvarchar(50)
-)
+CREATE TABLE [dbo].[Colors] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [ColorName] NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Colors] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
 
-CREATE TABLE Customers(
-	Id int PRIMARY KEY IDENTITY(1,1),
-	UserId int,
-	CompanyName nvarchar(50),
-	FOREIGN KEY (UserId) REFERENCES Users(Id)
-)
+CREATE TABLE [dbo].[Customers] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]      INT           NULL,
+    [CompanyName] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Rentals] (
+    [Id]         INT      IDENTITY (1, 1) NOT NULL,
+    [CarId]      INT      NULL,
+    [CustomerId] INT      NULL,
+    [RentDate]   DATETIME NULL,
+    [ReturnDate] DATETIME NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([CarId]) REFERENCES [dbo].[Cars] ([Id]),
+    FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([Id])
+);
+
+CREATE TABLE [dbo].[Users] (
+    [Id]           INT             IDENTITY (1, 1) NOT NULL,
+    [FirstName]    VARCHAR (50)    NOT NULL,
+    [LastName]     VARCHAR (50)    NOT NULL,
+    [Email]        VARCHAR (50)    NOT NULL,
+    [PasswordHash] VARBINARY (500) NOT NULL,
+    [PasswordSalt] VARBINARY (500) NOT NULL,
+    [Status]       BIT             NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[OperationClaims] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (250) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[UserOperationClaims] (
+    [Id]               INT IDENTITY (1, 1) NOT NULL,
+    [UserId]           INT NOT NULL,
+    [OperationClaimId] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([OperationClaimId]) REFERENCES [dbo].[OperationClaims] ([Id]),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+);
 
 
-CREATE TABLE Rentals(
-	Id int PRIMARY KEY IDENTITY(1,1),
-	CarId int,
-	CustomerId int,
-	RentDate datetime,
-	ReturnDate datetime,
-	FOREIGN KEY (CarId) REFERENCES Cars(Id),
-	FOREIGN KEY (CustomerId) REFERENCES Customers(Id)
